389199ae88b9488e08c3326ac4671f2e3542b9b5
Mingzhi Lin <mingzhi9@gmail.com>
2016-08-16 17:29:43 -0400

    change cfg_create

--- cmd/simmlst_cfg_create/cfg_create.go
+++ cmd/simmlst_cfg_create/cfg_create.go
@@ -2,26 +2,38 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
+	"fmt"
+	"github.com/alecthomas/kingpin"
 	"github.com/mingzhi/simmlst"
 	"os"
 )
 
-func main() {
-	var input, output string
-	var rep int
-	var par ParameterSet
-	var cfgs []simmlst.Config
-	flag.IntVar(&rep, "r", 1, "replicates")
-	flag.Parse()
-	input = flag.Arg(0)
-	output = flag.Arg(1)
+var (
+	cfgFile  = kingpin.Arg("cfg", "configs file").String()
+	prefix   = kingpin.Flag("prefix", "output prefix").Default("test").String()
+	ppn      = kingpin.Flag("ppn", "ppn").Default("1").Int()
+	walltime = kingpin.Flag("walltime", "walltime").Default("48").Int()
+	message  = kingpin.Flag("message", "message").Default("a").String()
+	exec     = kingpin.Flag("exec", "exec name").Default("simmlst_corr").String()
+	repeat   = kingpin.Flag("repeat", "repeat").Default("100").Int()
+	ncpu     = kingpin.Flag("ncpu", "num of cpu").Default("1").Int()
+)
 
-	par = parse(input)
-	cfgs = create(par, rep)
-	write(output, cfgs)
+func main() {
+	kingpin.Parse()
+	ps := parse(*cfgFile)
+	cs := create(ps)
+
+	for _, c := range cs {
+		writeCfgJSON(c)
+		writeCfgIni(c)
+		writePbs(c)
+	}
+	writeCfgs(cs)
+	writeQSub(cs)
 }
 
+// ParameterSet stores a set of parameters.
 type ParameterSet struct {
 	Sizes    []int
 	NumGenes []int
@@ -31,7 +43,7 @@ type ParameterSet struct {
 	Deltas   []int
 }
 
-func create(par ParameterSet, rep int) []simmlst.Config {
+func create(par ParameterSet) []simmlst.Config {
 	var cfgs []simmlst.Config
 	for _, size := range par.Sizes {
 		for _, numGene := range par.NumGenes {
@@ -39,21 +51,24 @@ func create(par ParameterSet, rep int) []simmlst.Config {
 				for _, theta := range par.Thetas {
 					for _, rho := range par.Rhos {
 						for _, deltas := range par.Deltas {
-							for i := 0; i < rep; i++ {
-								var cfg simmlst.Config
-								cfg.N = size
-								cfg.NumGene = numGene
-								cfg.LenGene = lenGene
-								cfg.Theta = theta
-								cfg.Rho = rho
-								cfg.Delta = deltas
-								cfgs = append(cfgs, cfg)
-							}
+							var cfg simmlst.Config
+							cfg.N = size
+							cfg.NumGene = numGene
+							cfg.LenGene = lenGene
+							cfg.Theta = theta
+							cfg.Rho = rho
+							cfg.Delta = deltas
+							cfgs = append(cfgs, cfg)
 						}
 					}
 				}
 			}
 		}
+	}
+
+	// add output prefix
+	for i := 0; i < len(cfgs); i++ {
+		cfgs[i].Output = fmt.Sprintf("%s_individual_%d", *prefix, i)
 	}
 
 	return cfgs
@@ -87,4 +102,75 @@ func write(filename string, configs []simmlst.Config) {
 	if err := e.Encode(configs); err != nil {
 		panic(err)
 	}
+}
+
+func writeJSON(s interface{}, filename string) {
+	w, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer w.Close()
+
+	e := json.NewEncoder(w)
+	if err := e.Encode(s); err != nil {
+		panic(err)
+	}
+}
+
+func writeCfgJSON(cfg simmlst.Config) {
+	filename := cfg.Output + ".cfg.json"
+	writeJSON(cfg, filename)
+}
+
+func writeCfgIni(cfg simmlst.Config) {
+	filename := cfg.Output + ".cfg.ini"
+	w, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer w.Close()
+
+	w.WriteString(fmt.Sprintf("%s", cfg))
+}
+
+func writePbs(c simmlst.Config) {
+	filename := c.Output + ".pbs"
+	w, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer w.Close()
+
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	w.WriteString("#!/bin/bash\n")
+	w.WriteString(fmt.Sprintf("#PBS -N %s\n", c.Output))
+	w.WriteString(fmt.Sprintf("#PBS -l nodes=1:ppn=%d\n", *ppn))
+	w.WriteString(fmt.Sprintf("#PBS -l walltime=%d:00:00\n", *walltime))
+	w.WriteString(fmt.Sprintf("#PBS -M ml3365@nyu.edu\n"))
+	w.WriteString(fmt.Sprintf("#PBS -m %s\n", *message))
+	w.WriteString(fmt.Sprintf("cd %s\n", wd))
+	w.WriteString(fmt.Sprintf("%s %s %s --repeat=%d --ncpu=%d\n", *exec, c.Output+".cfg.json", c.Output+".cov.csv", *repeat, *ncpu))
+}
+
+func writeQSub(cfgs []simmlst.Config) {
+	filename := *prefix + "_qsub.sh"
+	w, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer w.Close()
+
+	w.WriteString("#!/bin/bash\n")
+	for _, c := range cfgs {
+		w.WriteString(fmt.Sprintf("qsub %s.pbs\n", c.Output))
+	}
+}
+
+func writeCfgs(cfgs []simmlst.Config) {
+	filename := *prefix + "_configs.json"
+	writeJSON(cfgs, filename)
 }
--- sim.go
+++ sim.go
@@ -1,15 +1,31 @@
 package simmlst
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
 	"strings"
 )
 
+// Config stores a set of population parameters.
 type Config struct {
 	Theta, Rho       float64
 	N, Delta         int
 	NumGene, LenGene int
+	Output           string
+}
+
+func (p Config) String() string {
+	var b bytes.Buffer
+
+	fmt.Fprintf(&b, "theta = %g\n", p.Theta)
+	fmt.Fprintf(&b, "rho = %g\n", p.Rho)
+	fmt.Fprintf(&b, "n = %d\n", p.N)
+	fmt.Fprintf(&b, "num_gene = %d\n", p.NumGene)
+	fmt.Fprintf(&b, "len_gene = %d\n", p.LenGene)
+	fmt.Fprintf(&b, "output = %s\n", p.Output)
+
+	return b.String()
 }
 
 func (p Config) parse() (options []string) {
@@ -27,6 +43,7 @@ func (p Config) parse() (options []string) {
 	return
 }
 
+// Exec run simmlst.
 func Exec(ps Config, tempfile string) {
 	var options []string
 	options = ps.parse()
